65ede787e3b3b484676b9fa2b74750add5837aa7
Dayne <dayne.filer@gmail.com>
2020-02-21 14:22:00 -0500

    Started working on depth wrapper; still need tested

---
+++ bio/samtools/depth/wrapper.py
@@ -0,0 +1,25 @@
+"""Snakemake wrapper for running samtools depth."""
+
+__author__ = "Dayne L Filer"
+__copyright__ = "Copyright 2020, Dayne L Filer"
+__email__ = "dayne.filer@gmail.com"
+__license__ = "MIT"
+
+from snakemake.shell import shell
+
+# Samtools takes additional threads through its option -@
+# One thread for samtools merge
+# Other threads are *additional* threads passed to the '-@' argument
+threads = "" if snakemake.threads <= 1 else " -@ {} ".format(snakemake.threads - 1)
+
+# check for optional bed file
+bed = snakemake.input.get("bed", "")
+    if bed:
+        bed = "-b {}".format(known)
+
+shell(
+    "samtools depth {threads} {snakemake.params} {bed} "
+    "-o {snakemake.output[0]} {snakemake.input}"
+)
+
+
