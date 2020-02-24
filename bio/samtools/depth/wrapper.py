73c4a22c90c90ab8c10956a170bb8f3b28b90d25
Dayne <dayne.filer@gmail.com>
2020-02-24 16:27:16 -0500

    samtools/depth does not take -@ argument

--- bio/samtools/depth/wrapper.py
+++ bio/samtools/depth/wrapper.py
@@ -7,15 +7,10 @@ __license__ = "MIT"
 
 from snakemake.shell import shell
 
-# Samtools takes additional threads through its option -@
-# One thread for samtools merge
-# Other threads are *additional* threads passed to the '-@' argument
-threads = "" if snakemake.threads <= 1 else " -@ {} ".format(snakemake.threads - 1)
-
 # check for optional bed file
 bed = "" if snakemake.bed == "" else "-b {}".format(snakemake.input.bed)
 
 shell(
-    "samtools depth {threads} {snakemake.params} {bed} "
+    "samtools depth {snakemake.params} {bed} "
     "-o {snakemake.output[0]} {snakemake.input.bams}"
 )
